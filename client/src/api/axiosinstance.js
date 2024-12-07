2c9cab810069498cb01cb4f8e537e21d35ed2288
omkaryadav293 <omkaryadav9998@gmail.com>
2024-12-07 15:19:15 +0530

    env setup for frontend

--- client/src/api/axiosinstance.js
+++ client/src/api/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.env.VITE_API_URL,
+    baseURL : process.env.VITE_API_URL || "http://localhost:5000",
     
 })
 
